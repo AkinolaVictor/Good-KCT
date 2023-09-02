4d21eea0c8ec7d76985b73cd031ba5b10a55ee4f
Boughous-startup <akinolavictor50@gmail.com>
2023-09-02 18:54:53 +0100

    analytics

--- controllers/user/userDailyAnalytics.js
+++ controllers/user/userDailyAnalytics.js
@@ -7,22 +7,40 @@ async function userDailyAnalytics(req, res){
     const analytics = req.body.data || {}
     const userID = req.body.userID
     const currentDate = req.body.currentDate
+    // const currentDate = "name"
     // const dataString = JSON.stringify(data)
     // console.log(currentDate, analytics);
     // remove from audience
-    
+
     const usageRef = doc(database, 'usageAnalytics', userID)
     await getDoc(usageRef).then(async(docsnap)=>{
         if(docsnap.exists()){
-            const data = {...docsnap.data()}
-            if(data[currentDate]){
-                data[currentDate].push(analytics)
+            let analyticsData = {...docsnap.data()}
+            // const dataToString = ""
+            if(!analyticsData[currentDate]){
+                const finalData = [analytics]
+                let dataToString = JSON.stringify(finalData)
+                analyticsData[currentDate] = dataToString
+                // console.log(1, dataToString);
+                // await updateDoc(usageRef, {[currentDate]: dataToString}).then((res)=>{console.log('done 2x');}).catch((err)=>{console.log('done3x', err);})
             } else {
-                data[currentDate] = [analytics]
+                const actualData = [...JSON.parse(analyticsData[currentDate])]
+                actualData.push(analytics)
+                let dataToString = JSON.stringify(actualData)
+                analyticsData[currentDate] = dataToString
+                // console.log(2, dataToString);
+                // await updateDoc(usageRef, {[currentDate]: dataToString}).then((res)=>{console.log('done 2');}).catch((err)=>{console.log('done3', err);})
             }
-            await updateDoc(usageRef, {...data})
+            await setDoc(usageRef, {...analyticsData})
+            // await updateDoc(usageRef, {[currentDate]: dataToString}).then((res)=>{
+            //     console.log('done 2');
+            // }).catch((err)=>{
+            //     console.log('done3', err);
+            // })
         } else{
-            setDoc(usageRef, {[currentDate]: [analytics]})
+            const finalData = [analytics]
+            const dataToString = JSON.stringify(finalData)
+            setDoc(usageRef, {[currentDate]: dataToString})
         } 
     }).then(()=>{
         res.send({successful: true})
